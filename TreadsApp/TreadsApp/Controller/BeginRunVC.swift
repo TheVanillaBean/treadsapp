9270aad99be830c2ce886122d1471ba6be558cc1
Alex Alimov <teambluepin@gmail.com>
2018-02-04 03:25:58 -0700

    Add previous run  view

--- TreadsApp/TreadsApp/Controller/BeginRunVC.swift
+++ TreadsApp/TreadsApp/Controller/BeginRunVC.swift
@@ -12,27 +12,55 @@ import MapKit
 class BeginRunVC: LocationVC {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var lastRunCloseBtn: UIButton!
+    @IBOutlet weak var paceLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var lastRunBGView: UIView!
+    @IBOutlet weak var lastRunStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         checkLocationAuthStatus()
         mapView.delegate = self
-        
-        print("here are my runs \(Run.getAllRuns())")
-        
+                
     }
     
     override func viewWillAppear(_ animated: Bool) {
         manager?.delegate = self
         manager?.startUpdatingLocation()
-        
+        getlastRun()
+    }
+    
+    
+    @IBAction func lastRunCloseBtnPressed(_ sender: Any) {
+        lastRunStackView.isHidden = true
+        lastRunBGView.isHidden = true
+        lastRunCloseBtn.isHidden = true
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
     
+    
+    func getlastRun() {
+        guard let lastRun = Run.getAllRuns()?.first else {
+            lastRunStackView.isHidden = true
+            lastRunBGView.isHidden = true
+            lastRunCloseBtn.isHidden = true
+            return
+        }
+        
+        lastRunStackView.isHidden = false
+        lastRunBGView.isHidden = false
+        lastRunCloseBtn.isHidden = false
+        
+        paceLabel.text = lastRun.pace.formatTimeDurationToString()
+        distanceLabel.text = "\(lastRun.distance.metersToMiles(to: 2)) mi"
+        durationLabel.text = lastRun.duration.formatTimeDurationToString()
+    }
     
     @IBAction func locationCenterBtnPressed(_ sender: Any) {
         
